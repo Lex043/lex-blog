dfd45ce9935422198dc7b94895cb9c5c1b8f1708
Oyebamiji Alex <alexferanmi390@gmail.com>
2023-01-20 20:39:10 +0100

    feat: images to some of the blog post

--- src/pages/blog/{prismicPost.uid}.js
+++ src/pages/blog/{prismicPost.uid}.js
@@ -1,16 +1,19 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 import Seo from "../../components/Seo";
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.prismicPost.data.image);
   return (
     <Layout>
       <article className="md:mx-8">
         <h1 className="text-white mt-5 text-2xl font-bold">
           {data.prismicPost.data.title.text}
         </h1>
-        <p className="text-white text-xs">{data.prismicPost.data.date}.</p>
+        <p className="text-white text-xs pb-5">{data.prismicPost.data.date}.</p>
+        <GatsbyImage image={image} alt={data.prismicPost.data.content.text} />
         <p className="text-white my-5">{data.prismicPost.data.content.text}</p>
       </article>
     </Layout>
@@ -23,6 +26,9 @@ export const query = graphql`
       data {
         title {
           text
+        }
+        image {
+          gatsbyImageData(width: 300, placeholder: DOMINANT_COLOR)
         }
         date(formatString: "MMM DD, YYYY")
         content {
